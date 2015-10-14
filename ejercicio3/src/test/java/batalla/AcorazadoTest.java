8c9756eefe72b3e31564816c6990c23861e129cd
fabian <falago@gmail.com>
2015-10-14 13:54:18 -0300

    Test colocar Acorazado en lugar ocupado

--- ejercicio3/src/test/java/batalla/AcorazadoTest.java
+++ ejercicio3/src/test/java/batalla/AcorazadoTest.java
@@ -26,11 +26,33 @@ public class AcorazadoTest {
 		Acorazado acorazado = new Acorazado();
 		Assert.assertEquals("Posicion invalida", acorazado.agregarBarco(tablero, new Posicion(9, 9), new Vertical()));
 	}
+	
+	@Test
+	public void agregarHorizontalAcorazadoEnPoscionOcupada() {
+		inicializarTablero();
+		Acorazado acorazado1 = new Acorazado();
+		this.tablero[2][5]=acorazado1;
+		this.tablero[3][5]=acorazado1;
+		Acorazado acorazado2 = new Acorazado();
+		Assert.assertEquals("Posicion ocupada",
+				acorazado2.agregarBarco(tablero, new Posicion(3, 4), new Horizontal()));
+	}
+
+	@Test
+	public void agregarVerticalAcorazadoEnPoscionOcupada() {
+		inicializarTablero();
+		Acorazado acorazado1 = new Acorazado();
+		this.tablero[2][5]=acorazado1;
+		this.tablero[3][5]=acorazado1;
+		Acorazado acorazado2 = new Acorazado();
+		Assert.assertEquals("Posicion ocupada", acorazado2.agregarBarco(tablero, new Posicion(3, 5), new Vertical()));
+	}
+
 
 	private void inicializarTablero() {
-		for (int i = 0; i < tablero.length; i++) {
-			for (int j = 0; j < tablero[0].length; j++) {
-				tablero[i][j] = null;
+		for (int i = 0; i < this.tablero.length; i++) {
+			for (int j = 0; j < this.tablero[0].length; j++) {
+				this.tablero[i][j] = null;
 			}
 
 		}
